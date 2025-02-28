30584e9d376746d90f619d9d454f91e04c60e59d
Jong-Min <codejomo99@gmail.com>
2025-02-28 14:39:28 +0900

    init

---
+++ src/main/java/com/spring/sample/SampleController.java
@@ -0,0 +1,12 @@
+package com.spring.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+  @GetMapping("/sample")
+  public String test(){
+    return "hello world!";
+  }
+}
